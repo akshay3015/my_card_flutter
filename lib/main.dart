e8877accbbb0f394080a232425a2a95280200e0d
akshayshahane <akshay.shahane13@gmail.com>
2019-05-30 08:21:04 +0530

    completes UI design challenge

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,37 @@ class MyCardApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.00,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.00,
+                    height: 100.00,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.00,
+                    height: 100.00,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.00,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
